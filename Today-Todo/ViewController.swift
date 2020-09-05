972c27b75eb0ac8742c429fc03003a0ce7e21764
Alexander <hello@alexander.works>
2020-09-06 02:53:57 +0300

    Initial Commit

---
+++ Today-Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Today-Todo
+//
+//  Created by Alexander on 06.09.2020.
+//  Copyright © 2020 Alexander Nuzhnyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
